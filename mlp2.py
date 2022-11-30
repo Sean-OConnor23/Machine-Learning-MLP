0e87e76644d2cee489c6675100ebefcc4d5039c2
Emma Ingram <eci0004@auburn.edu>
2022-11-30 16:21:16 -0600

    Added different MLP

---
+++ mlp2.py
@@ -0,0 +1,180 @@
+from __future__ import print_function, division
+import numpy as np
+import math
+import pandas as pd
+
+def accuracy_score(y_true, y_pred):
+    """ Compare y_true to y_pred and return the accuracy """
+    accuracy = np.sum(y_true == y_pred, axis=0) / len(y_true)
+    return accuracy
+
+def normalize(X, axis=-1, order=2):
+    """ Normalize the dataset X """
+    l2 = np.atleast_1d(np.linalg.norm(X, order, axis))
+    l2[l2 == 0] = 1
+    return X / np.expand_dims(l2, axis)
+
+def to_categorical(x, n_col=None):
+    """ One-hot encoding of nominal values """
+    if not n_col:
+        n_col = np.amax(x) + 1
+    one_hot = np.zeros((x.shape[0], n_col))
+    one_hot[np.arange(x.shape[0]), x] = 1
+    return one_hot
+
+class Sigmoid():
+    def __call__(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def gradient(self, x):
+        return self.__call__(x) * (1 - self.__call__(x))
+
+class Softmax():
+    def __call__(self, x):
+        e_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
+        return e_x / np.sum(e_x, axis=-1, keepdims=True)
+
+    def gradient(self, x):
+        p = self.__call__(x)
+        return p * (1 - p)
+
+class Loss(object):
+    def loss(self, y_true, y_pred):
+        return NotImplementedError()
+
+    def gradient(self, y, y_pred):
+        raise NotImplementedError()
+
+    def acc(self, y, y_pred):
+        return 0
+
+class CrossEntropy(Loss):
+    def __init__(self): pass
+
+    def loss(self, y, p):
+        # Avoid division by zero
+        p = np.clip(p, 1e-15, 1 - 1e-15)
+        return - y * np.log(p) - (1 - y) * np.log(1 - p)
+
+    def acc(self, y, p):
+        return accuracy_score(np.argmax(y, axis=1), np.argmax(p, axis=1))
+
+    def gradient(self, y, p):
+        # Avoid division by zero
+        p = np.clip(p, 1e-15, 1 - 1e-15)
+        return - (y / p) + (1 - y) / (1 - p)
+
+
+class MultilayerPerceptron():
+    """Multilayer Perceptron classifier. A fully-connected neural network with one hidden layer.
+    Unrolled to display the whole forward and backward pass.
+    Parameters:
+    -----------
+    n_hidden: int:
+        The number of processing nodes (neurons) in the hidden layer. 
+    n_iterations: float
+        The number of training iterations the algorithm will tune the weights for.
+    learning_rate: float
+        The step length that will be used when updating the weights.
+    """
+    def __init__(self, n_hidden, n_iterations=3000, learning_rate=0.01):
+        self.n_hidden = n_hidden
+        self.n_iterations = n_iterations
+        self.learning_rate = learning_rate
+        self.hidden_activation = Sigmoid()
+        self.output_activation = Softmax()
+        self.loss = CrossEntropy()
+
+    def _initialize_weights(self, X, y):
+        n_samples, n_features = X.shape
+        _, n_outputs = y.shape
+        # Hidden layer
+        limit   = 1 / math.sqrt(n_features)
+        self.W  = np.random.uniform(-limit, limit, (n_features, self.n_hidden))
+        self.w0 = np.zeros((1, self.n_hidden))
+        # Output layer
+        limit   = 1 / math.sqrt(self.n_hidden)
+        self.V  = np.random.uniform(-limit, limit, (self.n_hidden, n_outputs))
+        self.v0 = np.zeros((1, n_outputs))
+
+    def fit(self, X, y):
+
+        self._initialize_weights(X, y)
+
+        for i in range(self.n_iterations):
+
+            # ..............
+            #  Forward Pass
+            # ..............
+
+            # HIDDEN LAYER
+            hidden_input = X.dot(self.W) + self.w0
+            hidden_output = self.hidden_activation(hidden_input)
+            # OUTPUT LAYER
+            output_layer_input = hidden_output.dot(self.V) + self.v0
+            y_pred = self.output_activation(output_layer_input)
+
+            # ...............
+            #  Backward Pass
+            # ...............
+
+            # OUTPUT LAYER
+            # Grad. w.r.t input of output layer
+            grad_wrt_out_l_input = self.loss.gradient(y, y_pred) * self.output_activation.gradient(output_layer_input)
+            grad_v = hidden_output.T.dot(grad_wrt_out_l_input)
+            grad_v0 = np.sum(grad_wrt_out_l_input, axis=0, keepdims=True)
+            # HIDDEN LAYER
+            # Grad. w.r.t input of hidden layer
+            grad_wrt_hidden_l_input = grad_wrt_out_l_input.dot(self.V.T) * self.hidden_activation.gradient(hidden_input)
+            grad_w = X.T.dot(grad_wrt_hidden_l_input)
+            grad_w0 = np.sum(grad_wrt_hidden_l_input, axis=0, keepdims=True)
+
+            # Update weights (by gradient descent)
+            # Move against the gradient to minimize loss
+            self.V  -= self.learning_rate * grad_v
+            self.v0 -= self.learning_rate * grad_v0
+            self.W  -= self.learning_rate * grad_w
+            self.w0 -= self.learning_rate * grad_w0
+
+    # Use the trained model to predict labels of X
+    def predict(self, X):
+        # Forward pass:
+        hidden_input = X.dot(self.W) + self.w0
+        hidden_output = self.hidden_activation(hidden_input)
+        output_layer_input = hidden_output.dot(self.V) + self.v0
+        y_pred = self.output_activation(output_layer_input)
+        return y_pred
+
+
+def main():
+    # read data
+    df = pd.read_csv("data/features_30_sec.csv")
+    # shuffle data
+    df = df.sample(frac=1).reset_index(drop=True)
+    X = df.iloc[:,1:-1].to_numpy()
+    X = normalize(X)
+    y = df['label']
+    y = y.replace(['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock'], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    y = to_categorical(y.to_numpy())
+
+    # split train and test data 80/20
+    X_train = X[:800]
+    y_train = y[:800]
+
+    X_test = X[800:]
+    y_test = y[800:]
+
+    # MLP
+    clf = MultilayerPerceptron(n_hidden=16,
+        n_iterations=1000,
+        learning_rate=0.01)
+
+    clf.fit(X_train, y_train)
+    y_pred = np.argmax(clf.predict(X_test), axis=1)
+    y_test = np.argmax(y_test, axis=1)
+
+    accuracy = accuracy_score(y_test, y_pred)
+    print ("Accuracy:", accuracy)
+
+if __name__ == "__main__":
+    main()
